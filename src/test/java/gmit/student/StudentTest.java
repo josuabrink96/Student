d5e58e10b5058eb1cce02e954af337ff73444bdc
JOSUA BRINK - STUDENT <G00362643@gmit.ie>
2022-03-02 09:46:18 +0000

    testing name

--- src/test/java/gmit/student/StudentTest.java
+++ src/test/java/gmit/student/StudentTest.java
@@ -1,4 +1,25 @@
 package gmit.student;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class StudentTest {
+    @Test
+    void testNameAddress()
+    {
+        String expectedName = "Name";
+        Student s = new Student("Name", "emailaddress@email.com");
+        assertEquals(expectedName, s.getName());
+    }
+
+    @Test
+    void testMissingVariable()
+    {
+        String expectedMessage = "Missing name or email";
+        Exception ex = assertThrows(
+                IllegalArgumentException.class,
+                ()-> new Student("Missing Data")
+        );
+        assertEquals(expectedMessage, ex.getMessage());
+    }
 }
